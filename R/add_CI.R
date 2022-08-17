e8bfc5d7c08ffaa4b4782e0d4b440443dabfd7b2
Mark Baillie <bailliem@gmail.com>
2022-08-17 21:10:03 +0000

    version 0.3.1

--- R/add_CI.R
+++ R/add_CI.R
@@ -81,6 +81,7 @@ add_CI.ggsurvfit <- function(gg,
         ymin = est.lower,
         ymax = est.upper
       ),
+      stat = StatStepribbon,
       na.rm = TRUE,
       show.legend = FALSE
       ) +
